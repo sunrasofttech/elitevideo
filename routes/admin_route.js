61073bd799f36d6210fcc983b8d3e164bb789aab
surajbombale <surajbombale59@gmail.com>
2025-07-10 17:43:11 +0530

    added

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -7,7 +7,7 @@ const router = express.Router();
   router.post('/signup', AdminController.signUp);
   router.post('/login', AdminController.login);
   router.put('/:id',upload.single('profile_img'), AdminController.editAdmin);
-  router.get('/subadmins', AdminController.getAllSubAdmins);
+  router.post('/subadmins', AdminController.getAllSubAdmins);
   router.delete('/:id', AdminController.deleteAdmin);
   router.post('/:id', AdminController.getAdminById);
   router.put('/:id/change-password', AdminController.changePassword);
