59866972e3ec14e03c7a19aab5f01deefb0ad440
surajbombale <surajbombale59@gmail.com>
2025-07-23 17:44:13 +0530

    added

--- utils/upload.js
+++ utils/upload.js
@@ -40,7 +40,64 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 1000 * 1024 * 1024 },
+  limits: { fileSize: 3000 * 1024 * 1024 },
 });
 
 module.exports = upload;
+
+// const multer = require('multer');
+// const path = require('path');
+// const fs = require('fs');
+
+// Configure dynamic storage
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     let uploadPath = 'uploads/';
+
+//     if (file.mimetype.startsWith('video/')) {
+//       uploadPath = 'uploads/video/';
+//     }
+
+//     // Create folder if it doesn't exist
+//     fs.mkdirSync(uploadPath, { recursive: true });
+
+//     cb(null, uploadPath);
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, `${Date.now()}-${file.originalname}`);
+//   },
+// });
+
+// // File filter
+// const fileFilter = (req, file, cb) => {
+//   const allowedTypes = [
+//     'image/png',
+//     'image/jpeg',
+//     'image/svg+xml',
+//     'image/jpg',
+//     'text/csv',
+//     'application/vnd.android.package-archive',
+//     'video/mp4',
+//     'video/mkv',
+//     'video/webm',
+//     'audio/mpeg', 
+//     'audio/wav',
+//     'audio/aac',
+//     'audio/ogg',
+//     'audio/flac'
+//   ];
+
+//   if (allowedTypes.includes(file.mimetype)) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error('Invalid file type. Only images, CSV, APK, video, and audio files are allowed.'));
+//   }
+// };
+
+// const upload = multer({
+//   storage,
+//   fileFilter,
+//   limits: { fileSize: 1000 * 1024 * 1024 }, // 1000MB
+// });
+
+// module.exports = upload;
