d5dc6ead95d0f61044354f82ec0843b9ab43a1fb
surajbombale <surajbombale59@gmail.com>
2025-08-14 10:12:26 +0530

    Ad

--- controller/search_api_controller.js
+++ controller/search_api_controller.js
@@ -4,56 +4,30 @@ const ShortFilmModel = require('../model/short_film_model');
 const SeriesModel = require('../model/series_model');
 const SeasonEpisodeModel = require('../model/season_episode_model');
 const SeasonModel = require('../model/season_model');
-
 exports.searchAllContent = async (req, res) => {
     try {
         const { keyword } = req.query;
 
-        if (!keyword) {
-            return res.status(400).json({
-                status: false,
-                message: "Search keyword is required",
-            });
-        }
+        const movieWhere = keyword ? { movie_name: { [Op.like]: `%${keyword}%` } } : {};
+        const shortFilmWhere = keyword ? { short_film_title: { [Op.like]: `%${keyword}%` } } : {};
+        const seriesWhere = keyword ? { series_name: { [Op.like]: `%${keyword}%` } } : {};
+        const seasonEpisodeWhere = keyword ? { episode_name: { [Op.like]: `%${keyword}%` } } : {};
+        const seasonWhere = keyword ? { season_name: { [Op.like]: `%${keyword}%` } } : {};
 
-        const [movies, shortfilms, series, seasonepisode,season] = await Promise.all([
-            MovieModel.findAll({
-                where: {
-                    movie_name: { [Op.like]: `%${keyword}%` }
-                }
-            }),
-            ShortFilmModel.findAll({
-                where: {
-                    short_film_title: { [Op.like]: `%${keyword}%` }
-                }
-            }),
-            SeriesModel.findAll({
-                where: {
-                    series_name: { [Op.like]: `%${keyword}%` }
-                }
-            }),
-            SeasonEpisodeModel.findAll({
-                where: {
-                    episode_name: { [Op.like]: `%${keyword}%` }
-                }
-            }),
-            SeasonModel.findAll({
-                where: {
-                    season_name: { [Op.like]: `%${keyword}%` }
-                }
-            })
+        const [movies, shortfilms, series, seasonepisode, season] = await Promise.all([
+            MovieModel.findAll({ where: movieWhere }),
+            ShortFilmModel.findAll({ where: shortFilmWhere }),
+            SeriesModel.findAll({ where: seriesWhere }),
+            SeasonEpisodeModel.findAll({ where: seasonEpisodeWhere }),
+            SeasonModel.findAll({ where: seasonWhere })
         ]);
 
         return res.status(200).json({
             status: true,
-            message: "Search results fetched successfully",
-            data: {
-                movies,
-                shortfilms,
-                series,
-                seasonepisode,
-                season
-            }
+            message: keyword 
+                ? "Search results fetched successfully"
+                : "All content fetched successfully",
+            data: { movies, shortfilms, series, seasonepisode, season }
         });
     } catch (error) {
         return res.status(500).json({
